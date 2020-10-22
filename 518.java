e7b18b69179d90bc617efebbb26e77c45578fbf7
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2020-10-22 19:49:38 +0800

    Create 518.java

---
+++ 518.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[] = new int[amount+1];
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coin) {
+                    dp[j] = dp[j] + dp[j - coin];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
