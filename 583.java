8570a91a689f2edb4dbb7d745af8737360f3ffbe
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2020-11-21 15:46:21 +0800

    Create 583.java

---
+++ 583.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        int max = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                max = Math.max(dp[i][j], max);
+            }
+        }
+        return word1.length() + word2.length() - 2 * max;
+    }
+}
