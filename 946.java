ea1abd82f69acc5d0f10ddec784661a03e463811
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2020-12-02 17:44:12 +0800

    Create 946.java

---
+++ 946.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int j = 0;
+        for (int i = 0; i < pushed.length; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                j++;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
