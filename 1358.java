2d521d73a97012d51ae4b6ab5514294b6b6de357
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2020-12-18 20:43:42 +0800

    Create 1358.java

---
+++ 1358.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int res = 0;
+        int[] record = {-1, -1, -1}; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                res += Math.min(record[1], record[2]) + 1;
+            } else if (s.charAt(i) == 'b') {
+                res += Math.min(record[0], record[2]) + 1;
+            } else {
+                res += Math.min(record[0], record[1]) + 1;
+            }
+            record[s.charAt(i) - 'a'] = i;
+        }
+        return res;
+    }
+}
