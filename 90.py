d1cf2013159618d216725f9294aaa2ab95b24cc0
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-02-03 11:46:50 +0800

    Create 90.py

---
+++ 90.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        dic = {}
+        for i in nums:
+            dic[i] = dic.get(i, 0) + 1
+        res = [[]]
+        for i, v in dic.items():
+            temp = res.copy()
+            for j in res:
+                temp.extend(j+[i]*(k+1) for k in range(v))
+            res = temp
+        return res
