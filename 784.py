5e93cc1c4e1988aa6239115e215345f65c7c2584
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-02-14 11:40:14 +0800

    Create 784.py

---
+++ 784.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCasePermutation(self, S: str) -> List[str]:
+        if not S:
+            return ['']
+
+        res, tmp = [''], []
+
+        for ch in S:
+            if  '0' <= ch <= '9':
+                for item in res:
+                    tmp.append(item + ch)
+            else:
+                for item in res:
+                    tmp.append(item + ch.lower())
+                    tmp.append(item + ch.upper())
+            res, tmp = tmp, []
+        return res
