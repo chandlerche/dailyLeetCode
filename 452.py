f166541dfb972edc96562eb0bf4c1869c9f2229d
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-03-15 20:58:53 +0800

    Create 452.py

---
+++ 452.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        if len(points) == 0: return 0
+        points = sorted(points, key=lambda x: x[0])
+
+        start = points[0][0]
+        end = points[0][1]
+        res = 0
+
+        for point in points:
+            if point[0] <= end:
+                start = max(point[0], start)
+                end = min(point[1], end)
+            else:
+                res += 1
+                start = point[0]
+                end = point[1]
+        return res + 1
