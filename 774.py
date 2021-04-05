93eb5082aec62b5603528267df5a74517756ebc1
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-04-05 15:23:26 +0800

    Create 774.py

---
+++ 774.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minmaxGasDist(self, st, K):
+        """
+        :type stations: List[int]
+        :type K: int
+        :rtype: float
+        """
+        lf = 1e-6
+        rt = st[-1] - st[0]
+        eps = 1e-7
+        while rt - lf > eps:
+            mid = (rt + lf) / 2
+            cnt = 0
+            for a, b in zip(st, st[1:]):
+               cnt += (int)((b - a) / mid)
+            if cnt <= K: rt = mid
+            else: lf = mid
+        return rt
