7d69e3d7ec814996fb7a70e946b5b1c096a527e5
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-04-10 16:44:52 +0800

    Create 1028.py

---
+++ 1028.py
@@ -0,0 +1,20 @@
+class Solution:
+    def recoverFromPreorder(self, S: str) -> TreeNode:
+        ans = {-1: TreeNode(0)}    
+        def addTree(v, p):        
+            ans[p] = TreeNode(int(v))
+            if not ans[p - 1].left:
+                ans[p - 1].left = ans[p]
+            else:                  
+                ans[p - 1].right = ans[p]
+        val, dep = '', 0          
+        for c in S:
+            if c != '-':
+                val += c          
+            elif val:              
+                addTree(val, dep)   
+                val, dep = '', 1    
+            else:
+                dep += 1            
+        addTree(val, dep)        
+        return ans[0]
