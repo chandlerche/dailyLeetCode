861d4940a554cec02f1f0837716976222859654a
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-05-17 17:42:29 +0800

    Create 164.py

---
+++ 164.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) < 2:
+            return 0
+        min_val, max_val, n = float('inf'), float('-inf'), len(nums)
+        for i in range(n):
+            if nums[i] < min_val:
+                min_val = nums[i]
+            if nums[i] > max_val:
+                max_val = nums[i]
+
+        if min_val == max_val:
+            return 0
+
+        mins = [0] * (n + 1)
+        maxs = [0] * (n + 1)
+        has_num = [False] * (n + 1)
+
+        for num in nums:
+            index = int((num - min_val) * n / (max_val - min_val))
+            mins[index] = num if not has_num[index] else min(mins[index], num)
+            maxs[index] = num if not has_num[index] else max(maxs[index], num)
+            has_num[index] = True
+
+        max_len = 0
+        m = maxs[0]
+        for i in range(1, n + 1):
+            if has_num[i]:
+                curr_len = mins[i] - m
+                if curr_len > max_len:
+                    max_len = curr_len
+                m = maxs[i]
+
+        return max_len
