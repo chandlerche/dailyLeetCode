2b5cf054bebc175942b1e32832151936fe78c4b4
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-08-25 16:33:36 +0800

    Create 426.py

---
+++ 426.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def dfs(self, node):
+        '''
+        :param: Node
+        :return: head, tail
+        '''
+        head = node
+        tail = node
+        if node.left:
+            head, node.left = self.dfs(node.left)
+            node.left.right = node
+            head.left = None
+        if node.right:
+            node.right, tail = self.dfs(node.right)
+            node.right.left = node
+            tail.right = None
+        return head, tail
+        
+    def treeToDoublyList(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if root == None: return None
+        head, tail = self.dfs(root)
+        tail.right = head
+        head.left = tail
+        return head
