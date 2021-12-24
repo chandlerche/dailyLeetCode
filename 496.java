93642cccee9aedc95fdffcda186e40a20a232bc6
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2021-12-24 15:33:33 -0500

    Create 496.java

---
+++ 496.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<Integer>();
+        HashMap<Integer, Integer> hasMap = new HashMap<Integer, Integer>();
+        
+        int[] result = new int[nums1.length];
+        
+        for(int num : nums2) {
+            while(!stack.isEmpty() && stack.peek()<num){
+                hasMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        for(int i = 0; i < nums1.length; i++) result[i] = hasMap.getOrDefault(nums1[i], -1);
+            
+        return result;
+    }
+}
